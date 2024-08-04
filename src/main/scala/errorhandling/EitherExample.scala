e666d17030755681b4a9f1871d6dc54c61848e6c
Tom Fleurant <tom@flrnt.fr>
2024-08-04 10:12:32 +0200

    Chapter 4 - Either && Validated

---
+++ src/main/scala/errorhandling/EitherExample.scala
@@ -0,0 +1,44 @@
+package errorhandling
+
+import Either.*
+
+import scala.util.control.NonFatal
+
+object EitherExample:
+  def mean(xs: Seq[Double]): Either[String, Double] =
+    if xs.isEmpty then Left("mean of empty list!")
+    else Right(xs.sum / xs.length)
+
+  def safeDiv(x: Int, y: Int): Either[Throwable, Int] =
+    try Right(x / y)
+    catch case NonFatal(t) => Left(t)
+
+  def parseInsuranceRateQuote(
+      age: String,
+      numberOfSpeedingTickets: String,
+  ): Either[Throwable, Double] = for {
+    a <- Either.catchNonFatal(age.toInt)
+    tickets <- Either.catchNonFatal(numberOfSpeedingTickets.toInt)
+  } yield OptionExample.insuranceRateQuote(a, tickets)
+
+// Example of map2 usage for validation
+case class Name private (value: String)
+object Name:
+  def apply(name: String): Either[String, Name] =
+    if name.isBlank || name == null then Left("Name is empty.")
+    else Right(new Name(name))
+
+case class Age private (value: Int)
+object Age:
+  def apply(age: Int): Either[String, Age] =
+    if age < 0 then Left("Age is out of range.")
+    else Right(new Age(age))
+
+case class Person(name: Name, age: Age)
+case object Person:
+  def make(name: String, age: Int): Either[String, Person] =
+    Name(name).map2(Age(age))(Person(_, _))
+
+  // With error accumulation
+  def makeBoth(name: String, age: Int): Either[List[String], Person] =
+    map2Both(Name(name), Age(age), Person(_, _))
---
+++ src/main/scala/errorhandling/Validated.scala
@@ -0,0 +1,32 @@
+package errorhandling
+
+enum Validated[+E, +A]:
+  case Valid(get: A)
+  case Invalid(errors: E)
+
+  def toEither: Either[E, A] = this match
+    case Validated.Valid(get)      => Either.Right(get)
+    case Validated.Invalid(errors) => Either.Left(errors)
+
+  def map[B](f: A => B): Validated[E, B] = this match
+    case Validated.Valid(get)      => Valid(f(get))
+    case Validated.Invalid(errors) => Invalid(errors)
+
+  def map2[EE >: E, B, C](b: Validated[EE, B])(f: (A, B) => C)(combineErrors: (EE, EE) => EE): Validated[EE, C] =
+    (this, b) match
+      case (Valid(aa), Valid(bb))       => Valid(f(aa, bb))
+      case (Invalid(es), Valid(_))      => Invalid(es)
+      case (Valid(_), Invalid(es))      => Invalid(es)
+      case (Invalid(es1), Invalid(es2)) => Invalid(combineErrors(es1, es2))
+
+object Validated:
+  def fromEither[E, A](e: Either[E, A]): Validated[E, A] =
+    e match
+      case Either.Left(value)  => Invalid(value)
+      case Either.Right(value) => Valid(value)
+
+  def traverse[E, A, B](as: List[A], f: A => Validated[E, B], combineErrors: (E, E) => E): Validated[E, List[B]] =
+    as.foldRight(Valid(Nil): Validated[E, List[B]])((a, acc) => f(a).map2(acc)(_ :: _)(combineErrors))
+
+  def sequence[E, A](as: List[Validated[E, A]], combineErrors: (E, E) => E): Validated[E, List[A]] =
+    traverse(as, identity, combineErrors)
